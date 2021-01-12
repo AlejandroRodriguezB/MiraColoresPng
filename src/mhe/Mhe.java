1016e483580d2f288ab57d84e228c71aea70e1a0
Alejandro Rodríguez <32458197+AlejandroRodriguezB@users.noreply.github.com>
2021-01-12 18:34:06 +0100

    Add files via upload

---
+++ src/mhe/Mhe.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mhe;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+
+/**
+ *
+ * @author alumno
+ */
+public class Mhe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        InputStream i = new FileInputStream("SPACECONFONDO.png");
+        ImageInputStream ii = ImageIO.createImageInputStream(i);
+        BufferedImage iL= ImageIO.read(ii);
+        for(int o=0;o<iL.getHeight();o++){
+            System.out.print("DC.L ");
+            for (int j = 0; j <iL.getWidth(); j++) {
+                int srPix=iL.getRGB(o, j);
+                Color c = new Color(srPix);
+                int r= c.getRed();
+                int g=c.getBlue();
+                int b= c.getGreen();
+                System.out.print("$"+b+g+r+",");
+            }
+            System.out.println(" ");
+        
+        }
+        
+
+    }
+    
+}
